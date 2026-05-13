119111878c8fcbfe169074201b993e5d6c494eb2
linqin <yilf4399@gmail.com>
2026-05-13 19:55:07 +0800

    4.4修改启动日志，新增member日志包,修改logback日志

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -3,13 +3,20 @@ package com.jiawa.train.member.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
+    }
 }
