467153d45a71b387e7ff63594cf1ec1ba80bfeb7
linqin <yilf4399@gmail.com>
2026-05-18 10:52:32 +0800

    4.14 封装请求参数和返回结果

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
+import com.jiawa.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +18,14 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
 
+    // 统计会员总数，调用MemberMapper的countByExample方法，传入null表示统计所有会员
     public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
-    public long register(String mobile) {
+    // 注册方法，接收MemberRegisterReq对象，返回注册成功的会员ID
+    public long register(MemberRegisterReq req) {
+        String mobile = req.getMobile();
         MemberExample memberExample = new MemberExample();
         memberExample.createCriteria().andMobileEqualTo(mobile);
         List<Member> memberList = memberMapper.selectByExample(memberExample);
