0e33c07bb486515c991d50c3736372fc5a7c5625
linqin <yilf4399@gmail.com>
2026-05-19 17:07:50 +0800

    5.9 完成短信验证码登录功能

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -55,11 +55,17 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/send-code", {
-        mobile: loginForm.mobile,
+      axios.post("/member/member/send-code", {
+        mobile: loginForm.mobile
       }).then(response => {
-        console.log(response.data);
-      })
+        let data = response.data;
+        if (data.success) {
+          notification.success({ description: '发送验证码成功！' });
+          loginForm.code = "8888";
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
     };
 
     const login = () => {
