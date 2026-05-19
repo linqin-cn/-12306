40fb77377382f17df1697c70ab5569f6da71064e
linqin <yilf4399@gmail.com>
2026-05-19 16:57:43 +0800

    5.9 将后端接口改成body json方式传递参数，前后端联调成功

--- member/src/main/java/com/jiawa/train/member/controller/MemberController.java
+++ member/src/main/java/com/jiawa/train/member/controller/MemberController.java
@@ -9,12 +9,9 @@
     import jakarta.annotation.Resource;
     import jakarta.validation.Valid;
     import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.PostMapping;
-    import org.springframework.web.bind.annotation.RequestMapping;
-    import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.*;
 
-@RestController
+    @RestController
 @RequestMapping("/member")
 public class MemberController {
     @Resource
@@ -39,7 +36,7 @@ public class MemberController {
     }
 
     @PostMapping("/send-code")
-    public CommonResp<Long> sendCode(@Valid MemberSendCodeReq req) {
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
         memberService.sendCode(req);
         return new CommonResp<>();
     }
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -55,7 +55,7 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8080/memeber/member/send-code", {
+      axios.post("http://localhost:8000/member/member/send-code", {
         mobile: loginForm.mobile,
       }).then(response => {
         console.log(response.data);
