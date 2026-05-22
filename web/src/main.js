efaa2a71f7045203b048e737d4d7bec432b8ef2d
linqin <yilf4399@gmail.com>
2026-05-22 20:04:19 +0800

    6.9 为axios增加登录相关拦截，请求时带上token,返回时校验返回码是不是401

--- web/src/main.js
+++ web/src/main.js
@@ -2,17 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import  Antd from 'ant-design-vue'
-import * as Icons from '@ant-design/icons-vue'
-import axios from 'axios'
+import Antd, {notification} from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
+import './assets/js/enums';
 
-const app = createApp (App);
+const app = createApp(App);
 app.use(Antd).use(store).use(router).mount('#app');
 
-// 注册全局图标组件
+// 全局使用图标
 const icons = Icons;
-for (const icon in icons) {
-  app.component(icon, icons[icon]);
+for (const i in icons) {
+  app.component(i, icons[i]);
 }
 
 /**
@@ -20,11 +22,11 @@ for (const icon in icons) {
  */
 axios.interceptors.request.use(function (config) {
   console.log('请求参数：', config);
-  // const _token = store.state.member.token;
-  // if (_token) {
-  //   config.headers.token = _token;
-  //   console.log("请求headers增加token:", _token);
-  // }
+  const _token = store.state.member.token;
+  if (_token) {
+    config.headers.token = _token;
+    console.log("请求headers增加token:", _token);
+  }
   return config;
 }, error => {
   return Promise.reject(error);
@@ -34,17 +36,18 @@ axios.interceptors.response.use(function (response) {
   return response;
 }, error => {
   console.log('返回错误：', error);
-  // const response = error.response;
-  // const status = response.status;
-  // if (status === 401) {
-  //   // 判断状态码是401 跳转到登录页
-  //   console.log("未登录或登录超时，跳到登录页");
-  //   store.commit("setMember", {});
-  //   notification.error({ description: "未登录或登录超时" });
-  //   router.push('/login');
-  // }
+  const response = error.response;
+  const status = response.status;
+  if (status === 401) {
+    // 判断状态码是401 跳转到登录页
+    console.log("未登录或登录超时，跳到登录页");
+    store.commit("setMember", {});
+    notification.error({ description: "未登录或登录超时" });
+    router.push('/login');
+  }
   return Promise.reject(error);
 });
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 console.log('环境：', process.env.NODE_ENV);
 console.log('服务端：', process.env.VUE_APP_SERVER);
+
