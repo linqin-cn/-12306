b3e5bed331ab7bca820093cb180fbcb3d3375486
linqin <yilf4399@gmail.com>
2026-05-22 21:47:46 +0800

    7.2 增加乘车人新增接口

---
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -0,0 +1,33 @@
+package com.jiawa.train.member.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.jiawa.train.common.util.SnowUtil;
+import com.jiawa.train.member.domain.Passenger;
+import com.jiawa.train.member.mapper.PassengerMapper;
+import com.jiawa.train.member.req.PassengerSaveReq;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PassengerService {
+
+    @Resource
+    private PassengerMapper passengerMapper;
+
+    public void save(PassengerSaveReq req) {
+        DateTime now = DateTime.now();
+        Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        if (ObjectUtil.isNull(passenger.getId())) {
+//            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+    }
+}
