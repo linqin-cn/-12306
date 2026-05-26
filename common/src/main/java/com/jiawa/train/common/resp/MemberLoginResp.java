08c2eacc085d6578a299a3e9600c4dd5424f89d0
linqin <yilf4399@gmail.com>
2026-05-26 16:12:02 +0800

    7.5 使用线程本地变量存储会员信息

---
+++ common/src/main/java/com/jiawa/train/common/resp/MemberLoginResp.java
@@ -0,0 +1,52 @@
+package com.jiawa.train.common.resp;
+
+public class MemberLoginResp {
+    private Long id;
+    private String token;
+    private String mobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    /*@Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", mobile=").append(mobile);
+        sb.append("]");
+        return sb.toString();
+    }*/
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("MemberLoginResp{");
+        sb.append("id=").append(id);
+        sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", token='").append(token).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
