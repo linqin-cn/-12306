9195da5ea2417ecc388de28be733655ef65b4c57
linqin <yilf4399@gmail.com>
2026-05-28 16:40:35 +0800

    7.9 第七章完

--- member/src/main/java/com/jiawa/train/member/service/PassengerService.java
+++ member/src/main/java/com/jiawa/train/member/service/PassengerService.java
@@ -3,7 +3,10 @@ package com.jiawa.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.common.util.SnowUtil;
 import com.jiawa.train.member.domain.Passenger;
 import com.jiawa.train.member.domain.PassengerExample;
@@ -12,12 +15,16 @@ import com.jiawa.train.member.req.PassengerQueryReq;
 import com.jiawa.train.member.req.PassengerSaveReq;
 import com.jiawa.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PassengerService {
+    private static final Logger LOG = LoggerFactory.getLogger(PassengerService.class);
+
 
     @Resource
     private PassengerMapper passengerMapper;
@@ -61,4 +68,5 @@ public class PassengerService {
         pageResp.setList(list);
         return pageResp;
     }
+
 }
